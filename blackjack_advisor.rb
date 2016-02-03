55bc4e37b8ac146f415425a23163e180357ea7e4
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-03 16:55:39 -0500

    Oh god the changes there have been.  This really should have happened earlier. If i need to go back then good luck

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,4 +1,52 @@
-card_values = {"J" => 10, "Q" => "10", "K" => 10, "A" => 11}
+def is_integer?(value)
+  return value.to_i.to_s == value.to_s
+end
+
+def get_card_input_player(string, card_values)
+  puts "Enter value for #{string} card: "
+  temp = gets.chomp.upcase
+  if is_integer?(temp) && (temp.to_i >=2 && temp.to_i <= 10)
+    return temp.to_i
+  elsif card_values[temp]
+    return card_values[temp].to_i
+  else
+    puts "Please enter an actual card value, and remember use a number or respond with"
+    puts "J for Jack, Q for Queen, K for King or A for Ace."
+    return get_card_input(string)
+  end
+end
+
+def get_card_input_dealer(card_values, ace)
+  puts "Enter value for dealers card: "
+  temp = gets.chomp.upcase
+  if is_integer?(temp) && (temp.to_i >=2 && temp.to_i <= 10)
+    return temp.to_s
+  elsif card_values[temp] == ace
+    return "A"
+  elsif card_values[temp]
+    return card_values[temp].to_s
+  else
+    puts "Please enter an actual card value, and remember use a number or respond with"
+    puts "J for Jack, Q for Queen, K for King or A for Ace."
+    return get_card_input(string)
+  end
+end
+
+def get_hash_value(hash, index1, index2)
+  if hash[index1][index2]
+    return hash[index1][index2]
+  else
+    return hash.default
+  end
+end
+
+def show_best_move(move)
+  best_moves_key = {"S" => "Stand", "H" => "Hit", "P" => "Split",
+                    "Dh" => "Double if possible, otherwise Hit", "Ds" => "Double if possible, otherwise Stand"}
+  puts "Your best move is to #{best_moves_key[move]}"
+end
+
+card_values = {"J" => "10", "Q" => "10", "K" => "10", "A" => "11"}
 hard_table = {5 => {"2" => "H", "3" => "H", "4" => "H", "5" => "H", "6" => "H",
                     "7" => "H", "8" => "H", "9" => "H", "10" => "H", "A" => "H"},
               6 => {"2" => "H", "3" => "H", "4" => "H", "5" => "H", "6" => "H",
@@ -44,55 +92,24 @@ paired_table = {2 => {"8" => "H", "9" => "H", "10" => "H", "A" => "H"},
                 10 => {"2" => "S", "3" => "S", "4" => "S", "5" => "S", "6" => "S",
                       "7" => "S", "8" => "S", "9" => "S", "10" => "S", "A" => "S"}}
 
-def is_integer?(value)
-  return value.to_i.to_s == value.to_s
-end
-
-def get_hand_value(card1, card2, card_values)
-  if is_integer?(card1)
-    temp_card1 = card1.to_i
-  else
-    temp_card1 = get_card_value(card1)
-  end
-  if is_integer?(card2)
-    temp_card2 = card2.to_i
-  else
-    temp_card2 = get_card_value(card2)
-  end
-  return temp_card1 + temp_card2
-end
-
-def get_card_value(card1, card_values)
-  if is_integer?(card1)
-    return card1.to_i
-  elsif is_integer?(card_values[card1.to_s])
-    return card_values[card1.to_s.upcase]
-  end
-end
-
-def get_card_input(string, card_values)
-  puts "Enter value for #{string} card: "
-  temp = gets.chomp
-  if is_integer?(temp) && (temp.to_i >=2 && temp.to_i <= 10)
-    return temp
-  elsif card_values[temp]
-    return card_values[temp]
-  else
-    puts "Please enter an actual card value, and remember use a number or respond with"
-    puts "J for Jack, Q for Queen, K for King or A for Ace."
-    return get_card_input(string)
-  end
-end
-
+ACE = 11
 #set defaults for each Hash
-card_values.default = 0
 hard_table.default = "S"
 soft_table.default = "S"
 paired_table.default = "P"
 puts "I will be asking for your blackjack hand first one card at a time and then the dealers card."
 puts "Please respond with the number or for face cards enter J for Jack, Q for Queen, K for King or A for Ace."
-card1 = get_card_input("first", card_values)
-card2 = get_card_input("second", card_values)
-dealer = get_card_input("dealers", card_values)
+card1 = get_card_input_player("first", card_values)
+card2 = get_card_input_player("second", card_values)
+dealer = get_card_input_dealer(card_values, ACE)
+#this is prety bad, but i did not want to go through and change the "A"s in the hash to 11
+dealer = "A" if dealer.to_i == ACE
+#puts "Key: H = Hit, S = Stand, P = Split, Dh = double if you can or hold, Ds = double if you can or stand"
 
-puts "card 1 = #{card1}, card 2 = #{card2}, dealers card = #{dealer}"
+if card1 == card2
+  show_best_move(get_hash_value(paired_table, card1, dealer))
+elsif card1 == ACE || card2 == ACE
+  show_best_move(get_hash_value(soft_table, card1 + card2, dealer))
+else
+  show_best_move(get_hash_value(hard_table, card1 + card2,dealer))
+end
